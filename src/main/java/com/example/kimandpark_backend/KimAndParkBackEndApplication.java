5f36eb03d6408fc3c823db336af441593f0f324c
Daniel <byoung.mokk@gmail.com>
2024-03-11 09:44:11 +0900

    Add: 프로젝트 생성

---
+++ src/main/java/com/example/kimandpark_backend/KimAndParkBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.example.kimandpark_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KimAndParkBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KimAndParkBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/kimandpark_backend/KimAndParkBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kimandpark_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KimAndParkBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
