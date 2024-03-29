6d6000b0f6ec41a5f49c37fac7e67e3272c8f341
Daniel <byoung.mokk@gmail.com>
2024-03-29 17:17:13 +0900

    ✨ Feat: 관리자 엔드포인트 분리

---
+++ src/main/java/com/example/kimandpark_backend/controller/AdminViewController.java
@@ -0,0 +1,22 @@
+package com.example.kimandpark_backend.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.kimandpark_backend.dto.ResponseDTO;
+
+
+@RestController
+@RequestMapping("/admin")
+public class AdminViewController {
+
+	@GetMapping
+	@PreAuthorize("hasRole('ADMIN')")
+	public ResponseEntity<Object> admin() {
+		return ResponseEntity.ok(ResponseDTO.body(true, "내가관리자다!", ""));
+	}
+
+} // end class
