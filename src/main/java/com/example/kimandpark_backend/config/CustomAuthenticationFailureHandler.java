c19b6805e0ef32199059d56c80d73d1718f9d1e2
Daniel <byoung.mokk@gmail.com>
2024-04-01 17:45:17 +0900

    ✨ Feat: 로그인 실패 시 응답 분기 처리

--- src/main/java/com/example/kimandpark_backend/config/CustomAuthenticationFailureHandler.java
+++ src/main/java/com/example/kimandpark_backend/config/CustomAuthenticationFailureHandler.java
@@ -1,12 +1,11 @@
 package com.example.kimandpark_backend.config;
 
 import java.io.IOException;
-import java.util.Locale;
 
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -14,13 +13,19 @@ public class CustomAuthenticationFailureHandler implements AuthenticationFailure
 
 	@Override
 	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
-		AuthenticationException exception) throws IOException, ServletException {
+		AuthenticationException exception) throws IOException {
 
 		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
 		response.setContentType("application/json; charset=utf-8");
-		response.getWriter().print("{\"success\": false, \"message\": \"정보가 올바르지 않습니다.\"}");
+
+		if (exception instanceof BadCredentialsException) {
+			response.getWriter().print("{\"success\": false, \"message\": \"로그인 정보가 맞지 않거나, 회원정보가 없습니다.\"}");
+		} else {
+			response.getWriter().print("{\"success\": false, \"message\": \"인증에 실패하였습니다. 고객센터로 문의 바랍니다.\"}");
+		}
+
 		response.getWriter().flush();
 
 	}
 
-}
+} // end class
--- src/main/java/com/example/kimandpark_backend/config/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/example/kimandpark_backend/config/CustomAuthenticationSuccessHandler.java
@@ -28,17 +28,11 @@ public class CustomAuthenticationSuccessHandler implements AuthenticationSuccess
 		Authentication authentication) throws IOException, ServletException {
 		Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
 
-		System.out.println(roles.toString());
-
 		if (roles.contains("ROLE_ADMIN")) {
 			response.sendRedirect("/admin");
 		} else if (roles.contains("ROLE_USER")) {
 			response.sendRedirect("/users");
-		} else {
-			response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
-			response.setContentType("application/json; charset=utf-8");
-			response.getWriter().print("{\"success\": false, \"message\": \"회원가입 먼저 진행해주세요.\", \"data\": \"\"}");
-			response.getWriter().flush();
 		}
 	}
-}
+
+} // end class
