324521a1e07dd1a10fe49abf2a71bc8547a98441
Daniel <byoung.mokk@gmail.com>
2024-07-05 13:44:05 +0900

    🩹Fix: thorws 목록 제거

--- src/main/java/com/example/kimandpark_backend/config/CustomAuthenticationSuccessHandler.java
+++ src/main/java/com/example/kimandpark_backend/config/CustomAuthenticationSuccessHandler.java
@@ -7,7 +7,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -25,7 +24,7 @@ import jakarta.servlet.http.HttpServletResponse;
 public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 	@Override
 	public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-		Authentication authentication) throws IOException, ServletException {
+		Authentication authentication) throws IOException {
 		Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
 
 		if (roles.contains("ROLE_ADMIN")) {
